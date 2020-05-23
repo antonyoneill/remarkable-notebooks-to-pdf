f3440273309b2b02d10727e354abe8ca192f18d1
Antony O'Neill <antony@ant-web.co.uk>
2020-05-23 18:24:14 +0100

    Initial args

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log('Hello World');
+import * as yargs from "yargs";
+
+const argv = yargs.options({
+  inputDir: {
+    type: "string",
+    required: true,
+    alias: "i",
+    describe: "path to remarkable notepads",
+  },
+  outputDir: {
+    type: "string",
+    required: true,
+    alias: "o",
+    describe: "path to contain notepad PDFs",
+  },
+  help: { type: "boolean", alias: "h" },
+}).argv;
