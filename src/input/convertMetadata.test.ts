ce9f677ef468b24c539a21c78c4cea5488e7310f
Antony O'Neill <antony@ant-web.co.uk>
2020-05-25 16:52:30 +0100

    Add metadata converter

---
+++ src/input/convertMetadata.test.ts
@@ -0,0 +1,43 @@
+import RemarkableMetadata from "../types/RemarkableMetadata";
+import convertMetadata from "./convertMetadata";
+
+describe("convertMetadata", () => {
+  it("can convert a Document", () => {
+    const input: RemarkableMetadata = {
+      id: "id",
+      lastModified: "lastModified",
+      parent: "parent",
+      type: "DocumentType",
+      visibleName: "Document Name",
+    };
+
+    const output = convertMetadata(input);
+
+    expect(output).toMatchObject({
+      id: input.id,
+      lastModified: input.lastModified,
+      parent: input.parent,
+      visibleName: input.visibleName,
+    });
+  });
+
+  it("can convert a Collection", () => {
+    const input: RemarkableMetadata = {
+      id: "id",
+      lastModified: "lastModified",
+      parent: "parent",
+      type: "CollectionType",
+      visibleName: "Document Name",
+    };
+
+    const output = convertMetadata(input);
+
+    expect(output).toMatchObject({
+      id: input.id,
+      lastModified: input.lastModified,
+      parent: input.parent,
+      visibleName: input.visibleName,
+      content: [],
+    });
+  });
+});
---
+++ src/input/convertMetadata.ts
@@ -0,0 +1,26 @@
+import RemarkableMetadata from "../types/RemarkableMetadata";
+import DocumentMetadata from "../types/DocumentMetadata";
+import CollectionMetadata from "../types/CollectionMetadata";
+
+const convertMetadata = (
+  input: RemarkableMetadata
+): DocumentMetadata | CollectionMetadata => {
+  if (input.type === "CollectionType") {
+    return {
+      id: input.id,
+      lastModified: input.lastModified,
+      parent: input.parent,
+      visibleName: input.visibleName,
+      content: [],
+    };
+  } else if (input.type === "DocumentType") {
+    return {
+      id: input.id,
+      lastModified: input.lastModified,
+      parent: input.parent,
+      visibleName: input.visibleName,
+    };
+  }
+};
+
+export default convertMetadata;
