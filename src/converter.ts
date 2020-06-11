dfbd7f1b4477958a7d0c47711f3e19203901d373
Antony O'Neill <antony@ant-web.co.uk>
2020-06-11 15:48:07 +0100

    Print tree

--- src/converter.ts
+++ src/converter.ts
@@ -1,10 +1,13 @@
 import Args from "./types/args";
 import getNotebooks from "./input/getNotebooks";
+import getMetadataTree from "./input/getMetadataTree";
 
 const Converter = async (args: Args): Promise<void> => {
   const notebooks = await getNotebooks(args.inputDir);
 
-  console.log(notebooks);
+  const tree = getMetadataTree(notebooks);
+
+  console.log(tree);
 };
 
 export default Converter;
